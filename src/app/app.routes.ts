eb0645122ed67f7e31ba3e3864fac2fc9ca3334f
marg4ryn <poweski.jan@gmail.com>
2026-06-29 21:20:34 +0200

    chore: add Vitest configuration and layout files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { WelcomePage } from './features/welcome-page/welcome-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: WelcomePage }];
---
+++ src/app/features/welcome-page/welcome-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-page',
+  imports: [],
+  templateUrl: './welcome-page.html',
+  styleUrl: './welcome-page.scss',
+})
+export class WelcomePage {}
